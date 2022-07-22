5674c6603c7b029d917c4ef7012d7c748212268a
LuisCarrilloP <jl.carrillopadron@gmail.com>
2022-07-22 18:26:54 -0500

    corrected production transport env

--- utils/email.utils.js
+++ utils/email.utils.js
@@ -13,7 +13,7 @@ class Email{
 
     //Connect to mail service
     newTransport(){
-        if(process.env.NODE_ENV === "development"){
+        if(process.env.NODE_ENV === "production"){
             return nodemailer.createTransport({
                 service: "SendGrid",
                 auth: {
